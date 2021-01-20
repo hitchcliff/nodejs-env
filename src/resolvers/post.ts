3e0f86de99a1d628d0675b449488c74f61594e30
Kevin Nacario <hitchcliff1@gmail.com>
2021-01-21 06:52:16 +0800

    first commit

---
+++ src/resolvers/post.ts
@@ -0,0 +1,48 @@
+import { Post } from "../entities/Post";
+import { MyContext } from "src/types";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
+
+@Resolver()
+export class PostResolver {
+  @Query(() => [Post])
+  async posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+    return await em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  async post(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return await em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title", () => String) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id", () => Int) id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+    if (!post) {
+      return null;
+    }
+
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+
+    return post;
+  }
+}
